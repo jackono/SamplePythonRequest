8df4598db7bf760036d0969edfb235329a10cdbd
Jackono <jackonofabella@gmail.com>
2019-11-07 18:40:53 +0800

    Sample connect IDCS

---
+++ idcs_connect.py
@@ -0,0 +1,126 @@
+import requests
+import json
+
+def __init__():
+    global URL
+    global auth_token
+
+    # open config file to retrieve needed settings
+    with open('config.json', 'r') as f:
+        conf = json.load(f)
+    URL = conf['BaseUrl']
+
+    # pass client id and secret as parameter to get auth token
+    auth_token = getAuth(conf['ClientId'], conf['ClientSecret'])
+
+
+    userName = 'jaspiom@example.com'
+    createFile = 'file1.json'
+    updateFile = 'update1.json'
+    
+    # Comment out the function you want to test
+
+    # createUser(createFile)
+    # print getUserId(userName)
+    updateUser(userName, updateFile)
+    # deleteUser(userName)
+
+def getAuth(clientId, clientSecret): 
+
+    data = {
+    'grant_type': 'client_credentials',
+    'scope': 'urn:opc:idm:__myscopes__'
+    }
+
+    # Request for auth token
+    response = requests.post(URL + '/oauth2/v1/token', data=data, verify=False, auth=(clientId, clientSecret))
+
+    # Parses response into JSON format
+    res = response.json()
+    
+    # Parse access token
+    auth_token = res['access_token']
+
+    return auth_token
+
+def createUser(createFile):
+
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + auth_token,
+    }
+
+    contents = open(createFile, "r")
+
+    # Convert file contents into JSON format
+    dataString = json.dumps(contents.read())
+    data = json.loads(dataString)
+
+    response = requests.post(URL + '/admin/v1/Users', headers=headers, data=data, verify=False)
+
+    # Error handling
+    if response.status_code == 201:
+        print response.json()['userName'] + ' has been created'
+    else:
+        print 'An error has occured\n'
+        print response.json()
+
+
+def getUserId(userName):
+    headers = {
+    'Authorization': 'Bearer ' + auth_token,
+    }
+    response = requests.get(URL + '/admin/v1/Users', headers=headers, verify=False)
+
+    res = response.json()
+    
+    # Error handling
+    if response.status_code == 200:
+        for userId in res['Resources']:
+            # print userId['userName'] //Comment out to print all users
+            if userId['userName'] == userName:
+                print 'ID for ' + userName + ' retrieved ' + userId['id']
+                return userId['id']
+    else:
+        print response.json()
+
+def updateUser(userName, updateFile):
+
+    userId = getUserId(userName)
+
+    headers = {
+    'Content-Type': 'application/scim+json',
+    'Authorization': 'Bearer '+ auth_token,
+    }
+
+    contents = open(updateFile, "r")
+
+    # Convert file contents into JSON format
+    dataString = json.dumps(contents.read())
+    data = json.loads(dataString)
+
+    response = requests.patch(URL + '/admin/v1/Users/' + userId, headers=headers, data=data, verify=False)
+    
+    # Error handling
+    if response.status_code == 200:
+        print userName + ' has been updated'
+    else:
+        print 'An error has occured\n'
+        print response.json()
+
+def deleteUser(userName):
+    headers = {
+    'Authorization': 'Bearer '+ auth_token,
+    }
+    userId = getUserId(userName)
+
+    response = requests.delete(URL + '/admin/v1/Users/' + userId, headers=headers)
+
+    # Error handling
+    if response.status_code == 204:
+        print userName + ' has been deleted'
+    else:
+        print 'An error has occured\n'
+        print response.json()
+
+__init__()
